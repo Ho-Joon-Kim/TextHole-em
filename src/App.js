177ceb91e3b56d67a892f41ece4377f19c2e4257
TitanV <a01098930999@gmail.com>
2021-03-31 16:06:07 +0900

    add - file

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import Typing from 'react-typing-animation';
+
+function App() {
+  return (
+    <div className="App-header">
+      <Typing>
+      <div className="main">
+      <Typing.Speed ms={70} />
+      <strong>Welcome to Texas Hold'em</strong><br/>
+      <Typing.Speed ms={10} />
+      <br/>
+      <br/>
+      TexasHoldem v1. 2. 0 alpha 1<br/>
+      site by TitanV<br/>
+      program by Chorong<br/>
+      <br/>
+      C:\>I want a DOWNLOAD<br/>
+      "Thanks for playing my game"<br/>
+      <br/>
+      C:\>How to use this program????<br/>
+      Just see this LINK<br/>
+      <br/>
+      <br/>
+      <Typing.Speed ms={1} />
+      MIT License<br/>
+      <br/>
+      Copyright (c) 2021 Console Hold'em<br/>
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:<br/>
+      <br/>
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.<br/>
+      <br/>
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.
+      </div>
+      </Typing>
+    </div>
+  );
+}
+
+export default App;
